ca4334ffc00440ae4aae0571d3fabf85a6fe6aea
dongmin <rlaehdals753@naver.com>
2022-06-27 23:19:17 +0900

    fix : header title change

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -16,7 +16,7 @@ const Header = () :JSX.Element => {
     
         <div className='header'>
             <Title>
-                <header>마 식 가2222222</header>
+                <header>마 식 가</header>
                 {/* <button type='button'>햄버거</button> */}
             </Title>
         </div>
