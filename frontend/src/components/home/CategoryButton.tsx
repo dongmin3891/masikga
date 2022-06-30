457f877518dec91ad4cb99a244f654c2b944b069
dongmin <rlaehdals753@naver.com>
2022-07-01 01:12:28 +0900

    fix : 기존에 button component에 있던 로직 CategoryList로 이동 및 props 전달

--- frontend/src/components/home/CategoryButton.tsx
+++ frontend/src/components/home/CategoryButton.tsx
@@ -1,40 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 // import styled from 'styled-components';
-import api from '../../api';
-import categoryList from '../../interfaces/categoryList';
 
-
-
-
-
-const CategoryButton = () :JSX.Element => {
-
-  const [category, setCategory] = useState<categoryList[]>([]);
-
-
-    useEffect(() => {
-        getCategoryList();
-    },[])
-
-    const getCategoryList = async () => {
-        const result  = await api.getCategoryList();
-        if(result.data.length > 0){
-            setCategory(result.data);
-        }
-    }
+const CategoryButton = ({id, categoryName}) :JSX.Element => {
 
   return (
-      
-    <div>
-        {
-            category.length > 0 && category !== undefined &&
-            category?.map((items) => (
-                <button type="button" key={items.id}>{items.categoryName}</button>
-            ))
-        }
-    </div>
-      
-    
+        <button type="button" key={id}>{categoryName}</button>
   );
 }
 
