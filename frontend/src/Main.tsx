a439215dc3296f4b52fe526a957c36a8e73002af
dongmin3891 <rlaehdals753@naver.com>
2022-07-10 14:05:49 +0900

    feat : Main에서 추가 할 기능 TODO 주석 추가

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -8,6 +8,9 @@ import LoginPage from './pages/login';
 
 const Main = () => {
 
+  // TODO : 로그인 기능 만들어서 메인쪽에서 로그인에 필요한 데이터가 없을 때
+  // 강제로 로그인 페이지로 이동하는 기능 구현해야함
+
   return (
     <>
         {/* <GlobalStyles />  */}
