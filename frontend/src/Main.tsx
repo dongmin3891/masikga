8625e382ea9fa3a11d97eff0432b7daca671d31e
dongmin <rlaehdals753@naver.com>
2022-07-23 01:31:12 +0900

    fix: 새로고침을 위한 리다이렉트 주석처리

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter,Switch, Route, Redirect} from 'react-router-dom';
+import {BrowserRouter,Switch, Route} from 'react-router-dom';
 import GlobalStyles from './GlobalStyles';
 import Header from './components/header';
 import LoginPage from './pages/login';
@@ -10,7 +10,7 @@ const Main = () => {
 
   // TODO : 로그인 기능 만들어서 메인쪽에서 로그인에 필요한 데이터가 없을 때
   // 강제로 로그인 페이지로 이동하는 기능 구현해야함
-
+  // console.log("location", location.pathname);
   return (
     <>
         {/* <GlobalStyles />  */}
@@ -23,7 +23,7 @@ const Main = () => {
                 <Route path='/login' component={LoginPage} />
                 <Route path='/home' component={Home}/>
             </Switch>
-            <Redirect to='/login' />
+            {/* <Redirect to='/login' /> */}
         </BrowserRouter>
     </>
   );
