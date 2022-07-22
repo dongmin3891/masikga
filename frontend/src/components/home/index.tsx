acd7742c786d21b4f6c4398edeb2234d4a144cfa
dongmin <rlaehdals753@naver.com>
2022-07-23 01:32:01 +0900

    feat: history push 작업중 에러 고치기

--- frontend/src/components/home/index.tsx
+++ frontend/src/components/home/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router';
 // import styled from 'styled-components';
 
 //  폴더구조 변경해야함
 const CategoryButton = ({ id, categoryName }) => {
+  const history = useHistory();
+  const categoryIdOnClick = (e) => {
+    history.replace({
+      pathname: '/home',
+      state: e.target
+    })
+    console.log("test", e.currentTarget)
+  }
 
   return (
-        <button type="button" key={id}>{categoryName}</button>
+        <button type="button" key={id} onClick={(e) =>categoryIdOnClick(e)} >{categoryName}</button>
   );
 }
 
