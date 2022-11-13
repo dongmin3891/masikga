1a0d8836aafe8f83194775df2a4e6ef40bc404f2
dongminkim <rlaehdals753@naver.com>
2022-11-13 17:53:16 +0900

    fix: menuList api kind 속성 보도록 수정

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -3,42 +3,55 @@ import CategoryList from "./category";
 import Menu from "./menu";
 import api from "../../api";
 import categoryList from "../../interfaces/categoryList";
+import CategoryButton from "../../components/home";
+
+
 
 const Home = () => {
-
+  //TODO: type 정의
   const [category, setCategory] = useState<categoryList[]>(null);
+  const [menuId, setMunuId] = useState('A'); 
 // 한번 더 테스트
   useEffect(() => {
+    // TODO: menuid 변경될 때마다 api 호출 아니면 rxjs맵도 사용해봐도 될듯
     getCategoryList();
-},[])
+  },[])
+
+  useEffect(() => {
+    console.log("menuId", menuId);
+  },[menuId])
+
 // pull request test
 const getCategoryList = async () => {
     const result = await api.getCategoryList();
     // TODO : 조건 api code로 변경
-    console.log(" categoryList :", result.data);
     if(result.retCode === "0000"){
         setCategory(result.data);
     }
+}
+
+const categoryOnClick = (e) => {
+  const menuId = e.target.menuId
+  setMunuId(menuId)
 }
 
 
 // 카테고리 리스트 조회 후 클릭마다 현재 아이디 menu로 프롭스 넘겨주기
 // 현재 클릭된 ID STATE에 담기
 // useRef 써보기
+// 여서 button으로 redering
   return (
     <>
         <div>
           메뉴를 골라줘
         </div>
-
         {
             category?.map((items) => (
-                <CategoryList key={items.id} id={items.id} categoryName={items.categoryName} />
+              <CategoryButton setMenuId={setMunuId} key={items.id} menuId={items.kind} categoryName={items.categoryName} onClick={categoryOnClick} />
             ))
-            // 리스트
+
         }
-        
-        <Menu />
+        <Menu menuId={menuId}/>
     </>
   )
 }
