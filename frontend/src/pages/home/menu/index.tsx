8f8896fbb88438f7052e54974c90f59d667bc673
dongminkim <rlaehdals753@naver.com>
2022-11-13 17:49:26 +0900

    feat: menuList API 불러와서 화면에 렌더되도록 로직 추가

--- frontend/src/pages/home/menu/index.tsx
+++ frontend/src/pages/home/menu/index.tsx
@@ -2,27 +2,46 @@ import React, { useEffect, useState } from "react";
 import api from "../../../api";
 import MenuListType from "../../../interfaces/menuList";
 
-const Menu = () => {
+interface IProps {
+  menuId: string;
+}
+
+const Menu = ( { menuId } : IProps) => {
 
     const [menuList, setMenuList] = useState<MenuListType[]>(null);
 
+    //menuId에 따른 menuList 조회
+    // post 요청
+    // 서버 api 확인
 	useEffect(() => {
-		getMenuList();
-	},[])
+		getMenuList(menuId);
+	},[menuId])
 
-    const getMenuList = async () => {
-		const result = await api.getMenuList();
-		if(result.retCode === "0000"){
-			setMenuList(result.data);
-		}
+  const getMenuList = async (menuId) => {
+    const result = await api.getMenuList(menuId);
+
+    if(result.retCode === "0000"){
+    console.log("result", result);
+      setMenuList(result.data);
     }
-
-	console.log("menuList", menuList);
-
+  }
 	return (
 		<div>
 			<h1>Menu Component</h1>
-		</div>
+      <div>
+        <ul>
+          {menuList?.map((items) => (
+            <li key={items.id}>
+              <h3>{items.storeName}</h3>
+              {/* <img alt=''>임시 이미지</img> */}
+              {items.foodName[0].name}
+              <div>{items.location}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+    
 	)
 }
 
