2777679591e99246cb2ceff00cfa8e88eb086cd2
dongmin3891 <rlaehdals753@naver.com>
2023-01-10 18:16:08 +0900

    aaa

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -5,7 +5,7 @@ const Header = () :JSX.Element => {
   return (
         <div className='header'>
             <S.Title>
-                <header>마 식 가 배포 테스트</header>
+                <header>마 식 가 배포</header>
                 <span></span>
                 {/* <button type='button'>햄버거</button> */}
             </S.Title>
