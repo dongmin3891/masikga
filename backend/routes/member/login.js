0925404da70f55b4b44703770c8cc97fd1fb1c10
dongmin3891 <rlaehdals753@naver.com>
2023-01-10 17:20:54 +0900

    feat: backend login api 추가

--- backend/routes/member/login.js
+++ backend/routes/member/login.js
@@ -1,19 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../schema/user')
 
 let data = {
-  retSysMsg : "SUCCESS",
-  data : {
-    userId: '',
-    password: '',
-  },
-  retCode: "0000",
-  retMsg: "SUCCESS",
+  retSysMsg : "",
+  retCode: "",
+  retMsg: "",
 }
 
-router.post('/', function(req, res, next) {
-  console.log("req member/login",req);
+router.post('/', async function(req, res, next) {
+  const { id, password } = req.body;
+  let user = await User.findOne({id});
+  console.log("findUser", user);
+  if(!user){
+    data.retCode = "0002"
+    data.retSysMsg = "SUCCESS"
+    data.retMsg = "존재하지 않는 ID입니다."
+    res.statusCode = 404;
+
+    return res.json(data);
+  }
+
+  if(user.password !== password){
+    data.retCode = "0003"
+    data.retSysMsg = "SUCCESS"
+    data.retMsg = "비밀번호가 일치하지 않습니다."
+    res.statusCode = 409;
+
+    return res.json(data);
+  }
+  data.retCode = "0000";
+  data.retSysMsg = "SUCCESS";
+  data.retMsg = "SUCCESS";
   res.statusCode = 200;
+
   res.json(data);
   // res.render('index', data );
 });
