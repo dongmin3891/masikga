f2b9893eb86bb9b0f0aad526275813688eefb123
dongmin3891 <rlaehdals753@naver.com>
2023-01-14 16:15:13 +0900

    fix: header 명 변경

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -5,7 +5,7 @@ const Header = () :JSX.Element => {
   return (
         <div className='header'>
             <S.Title>
-                <header>마 식 가 배포</header>
+                <header>마 식 가</header>
                 <span></span>
                 {/* <button type='button'>햄버거</button> */}
             </S.Title>
