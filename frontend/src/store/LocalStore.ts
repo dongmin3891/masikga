19ad87a0fea8c99ccb714b39d537c0bb743a062e
dongmin3891 <rlaehdals753@naver.com>
2023-01-14 16:08:19 +0900

    feat: localstorage에 토큰 저장 및 가져오기 유틸함수 추가

--- frontend/src/store/LocalStore.ts
+++ frontend/src/store/LocalStore.ts
@@ -14,3 +14,20 @@ export const gettingUserInfo = () => {
     return null;
   }
 }
+
+export const settingAuthToken = (token: string) => {
+  if(token){
+    window.localStorage.setItem('authToken', JSON.stringify(token))
+  } else {
+    window.localStorage.setItem('authToken', '');
+  }
+}
+
+export const gettingAuthToken = () => {
+  const token = window.localStorage.getItem('authToken');
+  if(token){
+    return JSON.parse(token);
+  }else {
+    return null;
+  }
+}
