374f39a1c306a0c3b41c51e4313a044b87ef4dcd
dongmin3891 <rlaehdals753@naver.com>
2023-01-14 16:07:22 +0900

    feat: cookie 핸들링 유틸 함수 추가

---
+++ frontend/src/utils/auth.ts
@@ -0,0 +1,10 @@
+import { Cookies } from 'react-cookie';
+const cookies = new Cookies();
+
+export const setRefreshTokenToCookie = (refresh_token) => {
+  cookies.set('refresh_token', refresh_token)
+}
+
+export const getRefreshTokenToCookie = () => {
+  return cookies.get('refresh_token');
+}
