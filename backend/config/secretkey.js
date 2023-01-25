5f9c4e6033ff15ef101cc123e1810032f4ffc932
dongmin3891 <rlaehdals753@naver.com>
2023-01-25 14:36:39 +0900

    fix: 토큰만료시간 변경

--- backend/config/secretkey.js
+++ backend/config/secretkey.js
@@ -2,12 +2,12 @@ module.exports = {
   secretkey : 'mysecretkey',
   accessOption : {
     algorithm : 'HS256', // 해싱 알고리즘
-    expiresIn : '1m', // 토큰 유효 기간
+    expiresIn : '30m', // 토큰 유효 기간
     issuer : "issuer" // 발행자 
   },
   refreshOption : {
     algorithm : 'HS256', // 해싱 알고리즘
-    expiresIn : '14d', // 토큰 유효 기간
+    expiresIn : '7d', // 토큰 유효 기간
     issuer : "issuer" // 발행자 
   }
 }
